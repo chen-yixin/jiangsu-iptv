03ac3c8b3ef046a366fc034796d22d821ddad49a
chen-yixin <chen-yixin@outlook.com>
2026-05-25 20:33:41 +0800

    Init uv project with basic script skeleton

---
+++ generate_iptv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""江苏IPTV直播源m3u8生成器"""
+
+import json
+import re
+import sys
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+import requests
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
